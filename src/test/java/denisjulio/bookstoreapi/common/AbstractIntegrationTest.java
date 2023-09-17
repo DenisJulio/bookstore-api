b29e7cd4a5bfa12a93d6c0655921f5b4bf5aca2c
Denis Julio <djms1234@gmail.com>
2023-09-17 13:16:42 -0300

    Fixes support for singleton containers

--- src/test/java/denisjulio/bookstoreapi/common/AbstractIntegrationTest.java
+++ src/test/java/denisjulio/bookstoreapi/common/AbstractIntegrationTest.java
@@ -1,17 +1,29 @@
 package denisjulio.bookstoreapi.common;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @SpringBootTest
-public class AbstractIntegrationTest {
+public abstract class AbstractIntegrationTest {
 
-  @Container
-  @ServiceConnection
   protected static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15");
 
+  @BeforeAll
+  protected static void beforeAll() {
+    postgres.start();
+  }
+
+  @AfterAll
+  static void afterAll() {
+    postgres.stop();
+  }
+
+  protected static void overridePropertiesInternal(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+  }
 }
--- src/test/java/denisjulio/bookstoreapi/domain/service/AuthorServiceTest.java
+++ src/test/java/denisjulio/bookstoreapi/domain/service/AuthorServiceTest.java
@@ -3,6 +3,8 @@ package denisjulio.bookstoreapi.domain.service;
 import denisjulio.bookstoreapi.common.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,9 +13,22 @@ class AuthorServiceTest extends AbstractIntegrationTest {
   @Autowired
   private AuthorService authorService;
 
+  @DynamicPropertySource
+  static void overrideProperties(DynamicPropertyRegistry registry) {
+    overridePropertiesInternal(registry);
+  }
+
   @Test
-  public void simpleTest() {
+  public void getAllAuthorsShouldGetCorrectAmountOfAuthors() {
     var authors = authorService.getAllAuthors();
-    assertThat(authors).isEmpty();
+    assertThat(authors).isNotEmpty();
+    assertThat(authors).hasSize(3);
+  }
+
+  @Test
+  void getAuthorByIdReturnCorrectAuthor() {
+    var author = authorService.getAuthorById(3);
+    assertThat(author.isPresent()).isTrue();
+    assertThat(author.get().getName()).isEqualTo("Author 3");
   }
 }
